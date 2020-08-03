0556b401d544d244d3af51bc506587a72465d068
SungHwa Jung <1404jsh@naver.com>
2020-08-03 09:27:31 +0900

    new

--- springedu/src/main/java/com/kh/portfolio/member/controller/RestMemberController.java
+++
@@ -1,8 +0,0 @@
-package com.kh.portfolio.member.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class RestMemberController {
-
-}
--- springedu/src/main/webapp/WEB-INF/resources/js/main.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-const openBtn = document.getElementById('openBtn');
-openBtn.addEventListener("click",(e)=>{
-	e.target.style.backgroundColor = 'yellow';
-}); 
